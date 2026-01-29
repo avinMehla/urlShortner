2ee6232f233fccfab0fc5c07addea305c0819169
Avin Mehla <Egselfish123@gmail.com>
2026-01-29 17:32:20 +0530

    @Service annotation
    Added comment to clarify the purpose of the @Service annotation.

--- src/main/java/com/example/demo/service/UrlService.java
+++ src/main/java/com/example/demo/service/UrlService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Service
+@Service // annotation helps in springboot
 public class UrlService {
 
     private final UrlRepo repo;
